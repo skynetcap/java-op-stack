9e34592863c6624c529804d0c617ebe497947101
skynetcap <100323448+skynetcap@users.noreply.github.com>
2023-05-30 13:38:47 -0700

    Add JUnit, SLF4J, Lombok dependencies

---
+++ src/test/java/OpStackTest.java
@@ -0,0 +1,12 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class OpStackTest {
+
+    @Test
+    public void opStackTest() {
+        log.info("Test ran");
+    }
+
+}
